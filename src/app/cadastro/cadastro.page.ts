cac47d65c119adc8e071d6a3bebc1f759769c348
Terceiros anos <danielamello.cedup.02.2022.@gmail.com>
2023-08-16 22:02:03 -0300

    Meu primeiro commit

---
+++ src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+})
+export class CadastroPage implements OnInit {
+public materia:string = '';
+public descricao: string='';
+public datahora:string='';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  salvar(){
+
+    alert('A materia digitada foi ' +this.materia);
+
+  }
+}
